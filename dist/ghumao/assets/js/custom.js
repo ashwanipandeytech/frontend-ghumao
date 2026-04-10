b53c4a122d0c2ce73d345233aead065c313f7078
Ashwani <ashwani.pandey@myqsoft.com>
2026-04-10 13:04:54 +0530

    Initial commit with correct .gitignore and deployment workflows

--- dist/ghumao/assets/js/custom.js
+++
@@ -1,9 +0,0 @@
-$('.package-slider').slick({
-    dots: true,
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    speed: 800,
-    slidesToShow: 3,
-    adaptiveHeight: false,
-});
